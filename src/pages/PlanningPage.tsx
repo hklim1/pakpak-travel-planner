60793034a068f19d45591a130ed6ca035271c6f7
hklim1 <xheatherlim@gmail.com>
2023-10-23 20:31:49 -0500

    Added mini-screen with moving triangles upon button press in Planning Page

--- src/pages/PlanningPage.tsx
+++ src/pages/PlanningPage.tsx
@@ -3,19 +3,91 @@ import DayPlan from '../components/DayPlan'
 
 export default function PlanningPage() {
   const [showLodging, setShowLodging] = useState(false);
+  const [showTransportation, setShowTransportation] = useState(false);
+  const [showActivities, setShowActivities] = useState(false);
+  const [showMap, setShowMap] = useState(false);
+
+  const onClickLodging = () => {
+    setShowLodging(true);
+    setShowTransportation(false);
+    setShowActivities(false);
+    setShowMap(false);
+  }
+  const onClickTransportation = () => {
+    setShowTransportation(true);
+    setShowLodging(false)
+    setShowActivities(false);
+    setShowMap(false);
+  }
+
+  const onClickActivities = () => {
+    setShowTransportation(false);
+    setShowLodging(false)
+    setShowActivities(true);
+    setShowMap(false);
+  }
+
+  const onClickMap = () => {
+    setShowTransportation(false);
+    setShowLodging(false)
+    setShowActivities(false);
+    setShowMap(true);
+  }
   
+  const Lodging = () => (
+    <>
+    <div className="triangle"></div>
+    <div className='planning-page-divs'>
+      <DayPlan date={new Date()} lodging='123 Abc St., Chicago, IL' />
+      <DayPlan date={new Date()} lodging='345 Xyz St., Irvine, CA' />
+      <DayPlan date={new Date()} lodging='789 Qrs St., St. Louis, MO' />
+    </div>
+    </>
+  )
+
+  const Transportation = () => (
+    <>
+    <div className='triangle-transport'></div>
+    <div className='planning-page-divs'>
+      <DayPlan date={new Date()} lodging='123 Abc St., Chicago, IL' />
+    </div>
+    </>
+  )
+
+  const Activities = () => (
+    <>
+    <div className='triangle-activities'></div>
+    <div className='planning-page-divs'>
+      <DayPlan date={new Date()} lodging='123 Abc St., Chicago, IL' />
+      <DayPlan date={new Date()} lodging='123 Abc St., Chicago, IL' />
+    </div>
+    </>
+  )
+
+  const Map = () => (
+    <>
+    <div className='triangle-map'></div>
+    <div className='planning-page-divs'>
+      <DayPlan date={new Date()} lodging='123 Abc St., Chicago, IL' />
+      <DayPlan date={new Date()} lodging='123 Abc St., Chicago, IL' />
+      <DayPlan date={new Date()} lodging='123 Abc St., Chicago, IL' />
+    </div>
+    </>
+  )
+
   return (
     <>
     <div className='planning-buttons'>
-      <button>LODGING</button>
-      <button>TRANSPORTATION</button>
-      <button>ACTIVITIES</button>
-      <button></button>
-      <div className='planning-page-divs'>
-        <DayPlan date={new Date()} lodging='123 Abc St., Chicago, IL' />
-        <DayPlan date={new Date()} lodging='345 Xyz St., Irvine, CA' />
-        <DayPlan date={new Date()} lodging='789 Qrs St., St. Louis, MO' />
-    </div>
+      <button onClick={onClickLodging}>LODGING</button>
+      <button onClick={onClickTransportation}>TRANSPORTATION</button>
+      <button onClick={onClickActivities}>ACTIVITIES</button>
+      <button onClick={onClickMap}>MAP</button>
+      <div className="mini-screen">
+        { showLodging && <Lodging /> }
+        { showTransportation && <Transportation/>}
+        { showActivities && <Activities />}
+        { showMap && <Map />}
+      </div>
     </div>
     </>
   )
