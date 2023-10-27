d3072a067d4b7ac75e10a28e87a24a20cbe48ce6
hklim1 <xheatherlim@gmail.com>
2023-10-27 01:22:03 -0500

    Fixed scrolling on Organize board and fixed  Create New Trip functionality

--- src/components/DragDrop/Organize.jsx
+++ src/components/DragDrop/Organize.jsx
@@ -7,20 +7,43 @@ import { Draggable } from "./Draggable";
 import { getUserTrip } from "../../firebaseUtils";
 import { useParams } from "react-router-dom";
 
+function timeDiff(startDate, endDate) {
+  const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+  const firstDate = new Date(endDate);
+  const secondDate = new Date(startDate);
+
+  const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay));
+
+  return diffDays;
+}
 
 export default function OrganizeDaySchedule() {
-  const containers = ["Sandbox", "Day 1", "Day 2", "Day 3"];
   const [locations, setLocations] = useState({}); // where each draggable is
   // const [parent, setParent] = useState(null);
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
   const [activities, setActivities] = useState([]);
   const { tripId } = useParams();
 
   useEffect(() => {
     getUserTrip(tripId).then((userData) => {
-      console.log(userData);
       setActivities(Object.values(userData["activities"]));
+      setStartDate(userData.startDate);
+      setEndDate(userData.endDate);
     });
   }, []);
+
+  const timeDifference = timeDiff(startDate, endDate);
+
+  function makeContainerArray(aTimeDiff) {
+    let container = [];
+    for (let i = 0; i < aTimeDiff; i++) {
+      container.push(`Day ${i + 1}`);
+    }
+    return container;
+  }
+
+  const containers = makeContainerArray(timeDifference);
 
   const draggables = activities.map((activity) => (
     <Draggable id={activity.activityId}>{activity.activityName}</Draggable>
@@ -28,21 +51,28 @@ export default function OrganizeDaySchedule() {
 
   return (
     <div className="overall-box">
-    <DndContext onDragEnd={handleDragEnd}>
-      {draggables.filter((d) => !locations[d.props.id])}
-      {containers.map((id) => (
-        // We updated the Droppable component so it would accept an `id`
-        // prop and pass it to `useDroppable`
-        <Droppable key={id} id={id}>
+      <DndContext onDragEnd={handleDragEnd}>
+        <Droppable key="Sandbox" id="Sandbox">
           <div className="header-top">
-          <h1 class="organize-headers">{id}</h1>
+            <h1 class="organize-headers">Sandbox</h1>
           </div>
           {draggables.filter(
-            (d) => locations[d.props.id] && locations[d.props.id] === id
+            (d) => !locations[d.props.id] || locations[d.props.id] === "Sandbox"
           )}
         </Droppable>
-      ))}
-    </DndContext>
+        {containers.map((id) => (
+          // We updated the Droppable component so it would accept an `id`
+          // prop and pass it to `useDroppable`
+          <Droppable key={id} id={id}>
+            <div className="header-top">
+              <h1 class="organize-headers">{id}</h1>
+            </div>
+            {draggables.filter(
+              (d) => locations[d.props.id] && locations[d.props.id] === id
+            )}
+          </Droppable>
+        ))}
+      </DndContext>
     </div>
   );
 
--- src/firebaseUtils.ts
+++ src/firebaseUtils.ts
@@ -1,260 +1,276 @@
 import { initializeApp } from "firebase/app";
-import { Timestamp, collection, doc, getDoc, getDocs, getFirestore, setDoc, updateDoc } from "firebase/firestore";
+import {
+  Timestamp,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  getFirestore,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC0ybQdzfihqslkgokEYgVi6KQP_mTn28Q",
-    authDomain: "pakpak-cebea.firebaseapp.com",
-    projectId: "pakpak-cebea",
-    storageBucket: "pakpak-cebea.appspot.com",
-    messagingSenderId: "347170407823",
-    appId: "1:347170407823:web:04b1d5f2ab8953925037fa"
-  };
-  
-  // Initialize Firebase
-  export const initFirebase = () => initializeApp(firebaseConfig);
-  
-  export const firebaseDB = getFirestore(initFirebase());
+  apiKey: "AIzaSyC0ybQdzfihqslkgokEYgVi6KQP_mTn28Q",
+  authDomain: "pakpak-cebea.firebaseapp.com",
+  projectId: "pakpak-cebea",
+  storageBucket: "pakpak-cebea.appspot.com",
+  messagingSenderId: "347170407823",
+  appId: "1:347170407823:web:04b1d5f2ab8953925037fa",
+};
 
-  export async function getFirebaseUser(userId: string) {
-    const docRef = doc(firebaseDB, 'Trips', userId);
-    const docSnap = await getDoc(docRef);
+// Initialize Firebase
+export const initFirebase = () => initializeApp(firebaseConfig);
 
-    if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
-      localStorage.setItem("tripData", JSON.stringify(docSnap.data()))
-    } else {
-      // docSnap.data() will be undefined in this case
-      await setDoc(doc(firebaseDB, "Trips", `${localStorage['token']}`), {
-        trips: []
-      });
-      localStorage.setItem("tripData", JSON.stringify({
-        trips: []
-      }));
-      console.log("Creating Doc for New User")
-    }
-  }
+export const firebaseDB = getFirestore(initFirebase());
 
-  export async function getAllUserTrips() {
-    const userId = localStorage['token']
-    const docRef = doc(firebaseDB, 'Trips', userId);
-    const docSnap = await getDoc(docRef);
-    const userData = docSnap.data()?.['trips']
-    return userData
-  }
+export async function getFirebaseUser(userId: string) {
+  const docRef = doc(firebaseDB, "Trips", userId);
+  const docSnap = await getDoc(docRef);
 
-  export async function getUserTrip(_tripId) {
-    const userId = localStorage['token']
-    const docRef = doc(firebaseDB, 'Trips', userId);
-    const docSnap = await getDoc(docRef);
-    const userData = docSnap.data()?.['trips']
-    
-    for (let i=0; i < userData.length; i++){
-      const tripId=userData[i]['tripId']
-      if (tripId === _tripId) {
-        console.log(userData[i])
-        return userData[i]
-      }
-    }
-  }
-
-  export async function addLodging(_tripId, lodgingData) {
-    const userId = localStorage['token']
-    const userTrips = await getAllUserTrips()
-    const userTrip = await getUserTrip(_tripId)
-    
-    const notImportantTrips: any = []
-    for (let i=0; i < userTrips.length; i++){
-      const tripId=userTrips[i]['tripId']
-      if (tripId != _tripId) {
-        notImportantTrips.push(userTrips[i])
-      }
-    }
-
-    userTrip['lodgings'] = {
-      ...userTrip['lodgings']
-    };
-
-    userTrip['lodgings'][lodgingData.lodgingId] = lodgingData;
-
-    const tripsRef = doc(firebaseDB, 'Trips', userId);
-
-    await updateDoc(tripsRef, {
-      trips: [...notImportantTrips, userTrip] 
-    })
-  }
-
-  export async function editLodging(_tripId, lodgingData) {
-    const lodgingId = lodgingData.lodgingId;
-    const userId = localStorage['token']
-    const userTrips = await getAllUserTrips()
-    const userTrip = await getUserTrip(_tripId)
-    const tripLodgings = userTrip['lodgings']
-    
-    const notImportantTrips: any = []
-
-    for (let i=0; i < tripLodgings.length; i++){
-      const tripId=userTrips[i]['tripId']
-      if (tripId != _tripId) {
-        notImportantTrips.push(userTrips[i])
-      }
-    }
-
-    userTrip['lodgings'][lodgingId] = {...lodgingData}
-
-    const tripsRef = doc(firebaseDB, 'Trips', userId);
-
-    await updateDoc(tripsRef, {
-      trips: [...notImportantTrips, userTrip] 
-    })
-  }
-
-  export async function deleteLodging(_tripId, lodgingId) {
-    // TO GET TRIP:
-    const userId = localStorage['token']
-    const userTrips = await getAllUserTrips()
-    const userTrip = await getUserTrip(_tripId)
-    const tripLodgings = userTrip['lodgings']
-    
-    const notImportantTrips: any = []
-
-    for (let i=0; i < tripLodgings.length; i++){
-      const tripId=userTrips[i]['tripId']
-      if (tripId != _tripId) {
-        notImportantTrips.push(userTrips[i])
-      }
-    }
-    // ALL THAT WAS FOR TRIP
-
-    // DELETE LODGING USING LODGING ID
-    delete userTrip['lodgings'][lodgingId];
-
-    // UPDATE TRIPS AFTER DELETION
-    const tripsRef = doc(firebaseDB, 'Trips', userId);
-
-    await updateDoc(tripsRef, {
-      trips: [...notImportantTrips, userTrip] 
-    })
-  }
-
-  // ======================= CRUD OPERATIONS FOR ACTIVITIES ============================
-
-  export async function addActivity(_tripId, activityData) {
-    const userId = localStorage['token']
-    const userTrips = await getAllUserTrips()
-    const userTrip = await getUserTrip(_tripId)
-    
-    const notImportantTrips: any = []
-    for (let i=0; i < userTrips.length; i++){
-      const tripId=userTrips[i]['tripId']
-      if (tripId != _tripId) {
-        notImportantTrips.push(userTrips[i])
-      }
-    }
-
-    userTrip['activities'] = {
-      ...userTrip['activities']
-    };
-
-    userTrip['activities'][activityData.activityId] = activityData;
-
-    const tripsRef = doc(firebaseDB, 'Trips', userId);
-
-    await updateDoc(tripsRef, {
-      trips: [...notImportantTrips, userTrip] 
-    })
-  }
-
-  export async function deleteActivity(_tripId, activityId) {
-    // TO GET TRIP:
-    const userId = localStorage['token']
-    const userTrips = await getAllUserTrips()
-    const userTrip = await getUserTrip(_tripId)
-    const tripActivities = userTrip['activities']
-    
-    const notImportantTrips: any = []
-
-    for (let i=0; i < tripActivities.length; i++){
-      const tripId=userTrips[i]['tripId']
-      if (tripId != _tripId) {
-        notImportantTrips.push(userTrips[i])
-      }
-    }
-
-    delete userTrip['activities'][activityId];
-
-    const tripsRef = doc(firebaseDB, 'Trips', userId);
-
-    await updateDoc(tripsRef, {
-      trips: [...notImportantTrips, userTrip] 
-    })
-  }
-
-  // ======================= CRUD OPERATIONS FOR ACTIVITIES ============================
-
-  export async function addVehicle(_tripId, vehicleData) {
-    const userId = localStorage['token']
-    const userTrips = await getAllUserTrips()
-    const userTrip = await getUserTrip(_tripId)
-    
-    const notImportantTrips: any = []
-    for (let i=0; i < userTrips.length; i++){
-      const tripId=userTrips[i]['tripId']
-      if (tripId != _tripId) {
-        notImportantTrips.push(userTrips[i])
-      }
-    }
-
-    userTrip['transportation'] = {
-      ...userTrip['transportation']
-    };
-
-    userTrip['transportation'][vehicleData.vehicleId] = vehicleData;
-
-    const tripsRef = doc(firebaseDB, 'Trips', userId);
-
-    await updateDoc(tripsRef, {
-      trips: [...notImportantTrips, userTrip] 
-    })
-  }
-
-  export async function deleteVehicle(_tripId, vehicleId) {
-    const userId = localStorage['token']
-    const userTrips = await getAllUserTrips()
-    const userTrip = await getUserTrip(_tripId)
-    const tripVehicles = userTrip['transportation']
-    
-    const notImportantTrips: any = []
-
-    for (let i=0; i < tripVehicles.length; i++){
-      const tripId=userTrips[i]['tripId']
-      if (tripId != _tripId) {
-        notImportantTrips.push(userTrips[i])
-      }
-    }
-
-    delete userTrip['transportation'][vehicleId];
-
-    const tripsRef = doc(firebaseDB, 'Trips', userId);
-
-    await updateDoc(tripsRef, {
-      trips: [...notImportantTrips, userTrip] 
-    })
-  }
-
-  // ================================ CREATE NEW TRIP ==========================================
-
-  export async function createNewTrip(_id, _tripName, _startDate, _endDate){
-    const newTrip = doc(firebaseDB, "Trips", `${localStorage['token']}`);
-    const allUsersTrips = await getAllUserTrips()
-    
-    allUsersTrips.push({tripId: _id,
-      tripName: _tripName,
-      startDate: _startDate,
-      endDate: _endDate})
-    
-      await updateDoc(newTrip, {
-      trips: allUsersTrips
-      
+  if (docSnap.exists()) {
+    console.log("Document data:", docSnap.data());
+    localStorage.setItem("tripData", JSON.stringify(docSnap.data()));
+  } else {
+    // docSnap.data() will be undefined in this case
+    await setDoc(doc(firebaseDB, "Trips", `${localStorage["token"]}`), {
+      trips: [],
     });
+    localStorage.setItem(
+      "tripData",
+      JSON.stringify({
+        trips: [],
+      })
+    );
+    console.log("Creating Doc for New User");
   }
+}
+
+export async function getAllUserTrips() {
+  const userId = localStorage["token"];
+  const docRef = doc(firebaseDB, "Trips", userId);
+  const docSnap = await getDoc(docRef);
+  const userData = docSnap.data()?.["trips"] ?? [];
+  return userData;
+}
+
+export async function getUserTrip(_tripId) {
+  const userId = localStorage["token"];
+  const docRef = doc(firebaseDB, "Trips", userId);
+  const docSnap = await getDoc(docRef);
+  const userData = docSnap.data()?.["trips"];
+
+  for (let i = 0; i < userData.length; i++) {
+    const tripId = userData[i]["tripId"];
+    if (tripId === _tripId) {
+      console.log(userData[i]);
+      return userData[i];
+    }
+  }
+}
+
+export async function addLodging(_tripId, lodgingData) {
+  const userId = localStorage["token"];
+  const userTrips = await getAllUserTrips();
+  const userTrip = await getUserTrip(_tripId);
+
+  const notImportantTrips: any = [];
+  for (let i = 0; i < userTrips.length; i++) {
+    const tripId = userTrips[i]["tripId"];
+    if (tripId != _tripId) {
+      notImportantTrips.push(userTrips[i]);
+    }
+  }
+
+  userTrip["lodgings"] = {
+    ...userTrip["lodgings"],
+  };
+
+  userTrip["lodgings"][lodgingData.lodgingId] = lodgingData;
+
+  const tripsRef = doc(firebaseDB, "Trips", userId);
+
+  await updateDoc(tripsRef, {
+    trips: [...notImportantTrips, userTrip],
+  });
+}
+
+export async function editLodging(_tripId, lodgingData) {
+  const lodgingId = lodgingData.lodgingId;
+  const userId = localStorage["token"];
+  const userTrips = await getAllUserTrips();
+  const userTrip = await getUserTrip(_tripId);
+  const tripLodgings = userTrip["lodgings"];
+
+  const notImportantTrips: any = [];
+
+  for (let i = 0; i < tripLodgings.length; i++) {
+    const tripId = userTrips[i]["tripId"];
+    if (tripId != _tripId) {
+      notImportantTrips.push(userTrips[i]);
+    }
+  }
+
+  userTrip["lodgings"][lodgingId] = { ...lodgingData };
+
+  const tripsRef = doc(firebaseDB, "Trips", userId);
+
+  await updateDoc(tripsRef, {
+    trips: [...notImportantTrips, userTrip],
+  });
+}
+
+export async function deleteLodging(_tripId, lodgingId) {
+  // TO GET TRIP:
+  const userId = localStorage["token"];
+  const userTrips = await getAllUserTrips();
+  const userTrip = await getUserTrip(_tripId);
+  const tripLodgings = userTrip["lodgings"];
+
+  const notImportantTrips: any = [];
+
+  for (let i = 0; i < tripLodgings.length; i++) {
+    const tripId = userTrips[i]["tripId"];
+    if (tripId != _tripId) {
+      notImportantTrips.push(userTrips[i]);
+    }
+  }
+  // ALL THAT WAS FOR TRIP
+
+  // DELETE LODGING USING LODGING ID
+  delete userTrip["lodgings"][lodgingId];
+
+  // UPDATE TRIPS AFTER DELETION
+  const tripsRef = doc(firebaseDB, "Trips", userId);
+
+  await updateDoc(tripsRef, {
+    trips: [...notImportantTrips, userTrip],
+  });
+}
+
+// ======================= CRUD OPERATIONS FOR ACTIVITIES ============================
+
+export async function addActivity(_tripId, activityData) {
+  const userId = localStorage["token"];
+  const userTrips = await getAllUserTrips();
+  const userTrip = await getUserTrip(_tripId);
+
+  const notImportantTrips: any = [];
+  for (let i = 0; i < userTrips.length; i++) {
+    const tripId = userTrips[i]["tripId"];
+    if (tripId != _tripId) {
+      notImportantTrips.push(userTrips[i]);
+    }
+  }
+
+  userTrip["activities"] = {
+    ...userTrip["activities"],
+  };
+
+  userTrip["activities"][activityData.activityId] = activityData;
+
+  const tripsRef = doc(firebaseDB, "Trips", userId);
+
+  await updateDoc(tripsRef, {
+    trips: [...notImportantTrips, userTrip],
+  });
+}
+
+export async function deleteActivity(_tripId, activityId) {
+  // TO GET TRIP:
+  const userId = localStorage["token"];
+  const userTrips = await getAllUserTrips();
+  const userTrip = await getUserTrip(_tripId);
+  const tripActivities = userTrip["activities"];
+
+  const notImportantTrips: any = [];
+
+  for (let i = 0; i < tripActivities.length; i++) {
+    const tripId = userTrips[i]["tripId"];
+    if (tripId != _tripId) {
+      notImportantTrips.push(userTrips[i]);
+    }
+  }
+
+  delete userTrip["activities"][activityId];
+
+  const tripsRef = doc(firebaseDB, "Trips", userId);
+
+  await updateDoc(tripsRef, {
+    trips: [...notImportantTrips, userTrip],
+  });
+}
+
+// ======================= CRUD OPERATIONS FOR ACTIVITIES ============================
+
+export async function addVehicle(_tripId, vehicleData) {
+  const userId = localStorage["token"];
+  const userTrips = await getAllUserTrips();
+  const userTrip = await getUserTrip(_tripId);
+
+  const notImportantTrips: any = [];
+  for (let i = 0; i < userTrips.length; i++) {
+    const tripId = userTrips[i]["tripId"];
+    if (tripId != _tripId) {
+      notImportantTrips.push(userTrips[i]);
+    }
+  }
+
+  userTrip["transportation"] = {
+    ...userTrip["transportation"],
+  };
+
+  userTrip["transportation"][vehicleData.vehicleId] = vehicleData;
+
+  const tripsRef = doc(firebaseDB, "Trips", userId);
+
+  await updateDoc(tripsRef, {
+    trips: [...notImportantTrips, userTrip],
+  });
+}
+
+export async function deleteVehicle(_tripId, vehicleId) {
+  const userId = localStorage["token"];
+  const userTrips = await getAllUserTrips();
+  const userTrip = await getUserTrip(_tripId);
+  const tripVehicles = userTrip["transportation"];
+
+  const notImportantTrips: any = [];
+
+  for (let i = 0; i < tripVehicles.length; i++) {
+    const tripId = userTrips[i]["tripId"];
+    if (tripId != _tripId) {
+      notImportantTrips.push(userTrips[i]);
+    }
+  }
+
+  delete userTrip["transportation"][vehicleId];
+
+  const tripsRef = doc(firebaseDB, "Trips", userId);
+
+  await updateDoc(tripsRef, {
+    trips: [...notImportantTrips, userTrip],
+  });
+}
+
+// ================================ CREATE NEW TRIP ==========================================
+
+export async function createNewTrip(_id, _tripName, _startDate, _endDate) {
+  const newTrip = doc(firebaseDB, "Trips", `${localStorage["token"]}`);
+  const allUsersTrips = await getAllUserTrips();
+
+  allUsersTrips.push({
+    tripId: _id,
+    tripName: _tripName,
+    startDate: _startDate,
+    endDate: _endDate,
+    activities: {},
+    lodgings: {},
+    transportation: {},
+  });
+
+  await updateDoc(newTrip, {
+    trips: allUsersTrips,
+  });
+}
